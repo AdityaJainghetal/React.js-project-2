a82d4991777b9b936b8763a1663547c55a69532d
Aditya Jain <adityajainghetal@gmail.com>
2025-12-04 16:36:58 +0530

    task

--- vite-project/src/Layout/Layout.jsx
+++ vite-project/src/Layout/Layout.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
-import Navbar from './Navbar';
-import { Outlet } from 'react-router-dom';
-import Footer from './Footer';
+import React from "react";
+import Navbar from "./Navbar";
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
 
 const Layout = () => {
   return (
-    <>
-      <Navbar />
-      <main>
+    <div className="flex flex-col min-h-screen">
+
+      {/* Navbar */}
+      <header className="w-full shadow-sm bg-white sticky top-0 z-50">
+        <Navbar />
+      </header>
+
+      {/* Main Content */}
+      <main className="flex-grow px-4 sm:px-6 lg:px-10 py-6 bg-gray-50">
         <Outlet />
       </main>
 
-      <Footer/>
-    </>
+      {/* Footer */}
+      <footer className="w-full bg-white border-t shadow-inner">
+        <Footer />
+      </footer>
+
+    </div>
   );
 };
 
